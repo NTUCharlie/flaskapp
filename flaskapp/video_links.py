e96043ba06773bd63e1d1a058f1fc52a778e6203
NTUCharlie <changlei199295@gmail.com>
2017-07-10 00:05:45 +0800

    Update video_links.py
    complete video_links

--- flaskapp/video_links.py
+++ flaskapp/video_links.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import urllib
 from http.cookiejar import CookieJar
-url = 'https://www.pornhub.com'
+url = 'https://www.pornhub.com/video'
 
 req = urllib.request.Request(url, headers={'ACCEPT':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                                          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
@@ -21,9 +21,36 @@ html_content = f.read().decode('utf-8')
 
 soup=BeautifulSoup(html_content,"lxml")
 all_a=soup.find_all('a')
+alllink=[]
 for link in all_a:
-    try:
-        if 'view_video' in link.get('href'):
-            print(link.get('href')+'\n')
-    except:
+    strlink = link.get('href')
+    a_string = link.string
+    if link.string!= None:
+        alllink.append([strlink,a_string])
+    else:
         continue
+
+def purify(list):
+    newlink=[]
+    for pair in list:
+        if "/view_video.php?" in str(pair[0]):
+            newlink.append(pair)
+        else:
+            continue
+    return newlink
+
+
+
+def get_id(strlink):
+    temlist=[]
+    temlist=strlink.split('=')
+    return str(temlist[-1])
+
+finallist=[]
+for item in purify(alllink):
+    finallist.append([get_id(item[0]),item[1]])
+print(finallist)
+
+
+
+
